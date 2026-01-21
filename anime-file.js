6b78a67973d8506ab3c074bf48975b5430d1c038
VMMAGIC <vmmagic000@gmail.com>
2026-01-21 13:06:46 +0530

    Add files via upload

---
+++ anime-file.js
@@ -0,0 +1,7 @@
+// Anime File Page JavaScript
+console.log('Anime File page loaded!');
+
+// Add any interactive functionality here
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM fully loaded');
+});
